75c8f0507293910627738717d94918ee9ac27d0a
reetou <fillmasterson@gmail.com>
2021-02-09 20:47:49 +0300

    Bump version and change readme

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule TinkoffInvest.MixProject do
   def project do
     [
       app: :tinkoff_invest,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
